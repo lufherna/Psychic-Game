26311fad75861f84d1cab53ff8eb2ab9cdc155ec
lufherna <lufherna@gmail.com>
2017-05-23 14:43:46 -0400

    added initial javascript and some span classes in the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,17 @@
 // random letter generator and variables down below
 
 
-var yourGuess = prompt('What letter am i thinking of? You have 10 guesses. Go!');
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var myGuess = alphabet(Math.floor(Math.random() * alphabet.length) + 1);
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l",
+"m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numberOfGuesses = 10;
-var wins = "0";
-var losses = "0";
+var wins = 0;
+var losses = 0;
 
-while()
+document.onkeyup = function(event){
+	var userGuess = event.key;
+	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+
+}
 
 </script>
