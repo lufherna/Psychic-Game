f9c0cfbafeb13a5b443917f78b9019431dd7e7dc
lufherna <lufherna@gmail.com>
2017-05-23 00:18:25 -0400

    added more details in the javascript variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,15 @@
 
 // random letter generator and variables down below
 
+
 var yourGuess = prompt('What letter am i thinking of? You have 10 guesses. Go!');
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var myGuess = alphabet(Math.floor(Math.random() * alphabet.length) + 1);
 var numberOfGuesses = 10;
 var wins = "0";
 var losses = "0";
+
+while()
 
 
 </script>
