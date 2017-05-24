0d082116d11f9bfed9d667c4a27ce4c5aa38b578
lufherna <lufherna@gmail.com>
2017-05-24 14:44:46 -0400

    code that resets the game after certain conditions are met

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,40 +7,65 @@ var numberOfGuesses = 10;
 var wins = 0;
 var losses = 0;
 var pressedKeys = [];
-
-//for loop
-
-	document.onkeyup = function runProgram(event){
-	var userGuess = event.key;
-	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+var computerGuess = '';
 
 
-//if user guesses right
-	if(userGuess === computerGuess){
-		wins++;
-				}
+//generate random letter
+function newRandom(){
 
-//if user guesses wrong
-	if(userGuess !== computerGuess) {
-
-		losses++;
-		numberOfGuesses--;
-	}
-//if user loses all his guesses
-	if(numberOfGuesses === 0) {
-
-		alert("Game Over Joker");
-
-	}
-	var holder = document.getElementById('user-wins');
-	holder.innerHTML = wins;
-	holder = document.getElementById('user-losses');
-	holder.innerHTML = losses;
-	holder = document.getElementById('guesses-left');
-	holder.innerHTML = numberOfGuesses;
-	holder = document.getElementById('lettersGuessed');
-	pressedKeys.push(userGuess);
-	holder.innerHTML = pressedKeys + ' ';
-
+	computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+	numberOfGuesses = 10
+	console.log('function ran, new letter = ' + computerGuess)
 }
 
+newRandom();
+
+
+document.onkeyup = function runProgram(event){
+
+	
+//if user loses all his guesses
+	if(numberOfGuesses > 0) {
+
+		var userGuess = event.key;
+
+			//if user guesses right
+		if(userGuess === computerGuess){
+			wins++;
+			
+		
+			newRandom();
+			}
+
+//if user guesses wrong
+		if(userGuess !== computerGuess) {
+		
+			numberOfGuesses--;
+		}
+
+		//varibles that hold values and will change the innerHTML on the DOM
+
+		var holder = document.getElementById('user-wins');
+		holder.innerHTML = wins;
+		holder = document.getElementById('user-losses');
+		holder.innerHTML = losses;
+		holder = document.getElementById('guesses-left');
+		holder.innerHTML = numberOfGuesses;
+		holder = document.getElementById('lettersGuessed');
+		pressedKeys.push(userGuess);
+		holder.innerHTML = pressedKeys;
+
+
+
+	} else {
+
+		losses++
+		alert("Game Over Joker");
+		numberOfGuesses = 10;
+		newRandom()
+		runProgram();
+	}
+
+
+
+}
