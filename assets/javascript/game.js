bed73cafbdc2dcf39a776f368400d1e71bb8bd82
lufherna <lufherna@gmail.com>
2017-05-23 23:32:04 -0400

    holder variable in javascript down below. Still cant get the program to show the number of guesses availabe

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,9 @@ var numberOfGuesses = 10;
 var wins = 0;
 var losses = 0;
 
-//main function 
+//for loop
 
-for (i = 0; i < alphabet.length; i++) {
+for (var i = 0; i < alphabet.length; i++) {
 
 document.onkeyup = function(event){
 	var userGuess = event.key;
@@ -18,23 +18,29 @@ document.onkeyup = function(event){
 //if user guesses right
 	if(userGuess === computerGuess){
 		wins++;
-		document.getElementById('user-Wins').innerHTML = numberOfGuesses;
 			}
 
 //if user guesses wrong
 	if(userGuess !== computerGuess) {
 
 		losses++;
-		numberOfGuesses-1;
-		document.getElementById('user-losses').innerHTML = losses;
-		document.getElementById('')
+		numberOfGuesses--;
 	}
-//if user loses
+//if user loses all his guesses
 	if(numberOfGuesses===0) {
 
 		alert("Game Over Joker");
 
 	}
+
+	var holder = document.getElementById('user-wins');
+	holder.innerHTML = wins;
+	holder = document.getElementById('user-losses');
+	holder.innerHTML = losses;
+	holder = document.getElementById('guesses-left');
+	holder.innerHTML = numberOfGuesses;
+	holder = document.getElementById('lettersGuessed');
+	holder.innerHTML = userGuess;
 
 }
 
